a5a355f3c126bd283ecdb86c6d9f74837e5923e4
angel <angelmf97@usal.es>
2019-06-07 11:54:45 +0200

    Help message changed.

--- sonar.py
+++ sonar.py
@@ -8,13 +8,12 @@ import argparse
 import xlsxwriter
 
 
-parser=argparse.ArgumentParser(description='SONAR is a Python based programme used to find, within any given set of proteins, the subset of them containing both repeated sequences and signal sequences. It makes use of the software RADAR and SignalP 4.0.')
-
-parser.add_argument('-f','--file',metavar='',type=argparse.FileType(),required=False,help='input file.')
-parser.add_argument('-r','--radar',action='store_true',required=False,help='prints RADAR\'s output')
-parser.add_argument('-t','--table',action='store_true',required=False,help='prints two tables containing the information of RADAR (RCPs only) and SignalP-4.1 (secreted RCPs)')
-parser.add_argument('-s','--signalp',action='store_true',required=False,help='prints SignalP\'s output')
-parser.add_argument('-o','--output',type=str,metavar='',required=True,help='destination directory.')
+parser=argparse.ArgumentParser(description='Sonar is a Python based script used to find, within any given set of proteins, the subset of them containing both repeated sequences and signal sequences, also known as secreted RCPs (repeat-containing proteins). It makes use of the programmes RADAR and SignalP 4.0.')
+parser.add_argument('-f','--file',metavar='',type=argparse.FileType(),required=True,help='input file.')
+parser.add_argument('-t','--table',action='store_true',required=False,help='prints two tables containing the information of RADAR (RCPs only) and SignalP-4.1 (secreted RCPs).')
+parser.add_argument('-r','--radar',action='store_true',required=False,help='prints the output of RADAR.')
+parser.add_argument('-s','--signalp',action='store_true',required=False,help='prints the output of SignalP 4.1.')
+parser.add_argument('-o','--output',type=str,metavar='',required=False,default='.',help='destination directory.')
 
 args=parser.parse_args()
 
